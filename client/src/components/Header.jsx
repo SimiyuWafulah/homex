fa7a3fc52892a0b90a545e4c1108147e7db8c66e
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-01-19 12:31:30 +0300

    created header component and page routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl p-3 mx-auto'>
+            <h1 className='text-sm sm:text-xl p-3 font-bold flex flex-wrap'>
+                <span className='text-slate-500'>Home-</span>
+                <span className='text-slate-700'>X</span>
+            </h1>
+            <form className='bg-slate-100 flex items-center rounded-lg '>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    className='rounded-lg p-3 focus:outline-none w-24 sm:w-64'
+                />
+                <FaSearch className='text-slate-700'/>
+            </form>
+            <ul className='flex gap-4'>
+                <Link to='/'>
+                    <li className='hidden sm:inline-block text-slate-700 hover:underline'>Home</li>
+                </Link>
+                <Link to='/about'>
+                    <li className='hidden sm:inline-block text-slate-700 hover:underline'>About</li>
+                </Link>
+                <Link to='/signin'>
+                    <li className=' sm:inline-block text-slate-700 hover:underline'>Sign In</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
