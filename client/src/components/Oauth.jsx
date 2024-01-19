6eba87ad27f3612dfae2c5bc343121b36557e381
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-01-19 12:48:40 +0300

    completed sign up ui

---
+++ client/src/components/Oauth.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Oauth() {
+  return (
+    <button className='bg-red-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase'>Continue with Google</button>
+  )
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Oauth from '../components/Oauth'
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className='max-w-lg mx-auto p-3'>
+     <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
+     <form className='flex flex-col gap-4'>
+      <input
+        type='text'
+        placeholder='username'
+        id='username'
+        className='border p-3 rounded-lg'
+      />
+      <input
+        type='email'
+        placeholder='email'
+        id='email'
+        className='border p-3 rounded-lg'
+      />
+      <input
+        type='password'
+        placeholder='password'
+        id='password'
+        className='border p-3 rounded-lg'
+      />
+      <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase'>Sign Up</button>
+      <Oauth/>
+     </form>
     </div>
   )
 }
